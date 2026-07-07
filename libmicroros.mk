168f5c96be72d762293ca98f753b07e33ebb76dc
karna923 <pushkarna923@gmail.com>
2026-07-07 00:34:24 -0500

    fix: WIFI_IF_STA rename and CONFIG_LIBC_NEWLIB flag for esp32 builds

--- libmicroros.mk
+++ libmicroros.mk
@@ -1,4 +1,4 @@
-EXTENSIONS_DIR = $(shell pwd)
+EXTENSIONS_DIR = $(subst \,/,$(CURDIR))
 UROS_DIR = $(EXTENSIONS_DIR)/micro_ros_src
 BUILD_DIR ?= $(EXTENSIONS_DIR)/build
 
@@ -40,7 +40,7 @@ $(EXTENSIONS_DIR)/micro_ros_dev/install:
 	git clone -b rolling https://github.com/ament/ament_index src/ament_index; \
 	touch src/ament_cmake_ros/rmw_test_fixture_implementation/COLCON_IGNORE; \
 	touch src/ament_cmake_ros/rmw_test_fixture/COLCON_IGNORE; \
-	colcon build --cmake-args -DBUILD_TESTING=OFF -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=gcc;
+        colcon build --cmake-args -DCMAKE_GENERATOR="Ninja" -DBUILD_TESTING=OFF;
 
 # ros2/rosidl needs to be pinned to an older version as ros2/rosidl#942 added rosidl_buffer as
 # a dependency for various rosidl packages and we can't build it currently.
@@ -95,13 +95,14 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 $(EXTENSIONS_DIR)/micro_ros_src/install: $(EXTENSIONS_DIR)/esp32_toolchain.cmake $(EXTENSIONS_DIR)/micro_ros_dev/install $(EXTENSIONS_DIR)/micro_ros_src/src
 	cd $(UROS_DIR); \
 	unset AMENT_PREFIX_PATH; \
-	PATH="$(subst /opt/ros/$(ROS_DISTRO)/bin,,$(PATH))"; \
+	export PATH="$(subst /opt/ros/$(ROS_DISTRO)/bin,,$(PATH))"; \
 	. ../micro_ros_dev/install/local_setup.sh; \
 	colcon build \
 		--merge-install \
 		--packages-ignore-regex=.*_cpp \
 		--metas $(EXTENSIONS_DIR)/colcon.meta $(APP_COLCON_META) \
 		--cmake-args \
+		-DCMAKE_GENERATOR="Ninja" \
 		"--no-warn-unused-cli" \
 		-DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=OFF \
 		-DTHIRDPARTY=ON \
@@ -110,7 +111,7 @@ $(EXTENSIONS_DIR)/micro_ros_src/install: $(EXTENSIONS_DIR)/esp32_toolchain.cmake
 		-DCMAKE_BUILD_TYPE=$(BUILD_TYPE) \
 		-DCMAKE_TOOLCHAIN_FILE=$(EXTENSIONS_DIR)/esp32_toolchain.cmake \
 		-DCMAKE_VERBOSE_MAKEFILE=OFF \
-        -DIDF_INCLUDES='${IDF_INCLUDES}' \
+        -DIDF_INCLUDES= ${IDF_INCLUDES} \
 		-DCMAKE_C_STANDARD=$(C_STANDARD) \
 		-DUCLIENT_C_STANDARD=$(C_STANDARD);
 
